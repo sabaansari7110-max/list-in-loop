6f23b485949e5b2633411cf09d8fde7e35867319
Saba Ansari <sabaansari7110@gmail.com>
2026-01-16 13:51:27 +0530

    Add files via upload

---
+++ list in loop.py
@@ -0,0 +1,10 @@
+#create a empty list 
+numbers= []
+
+#take 5 numbers from user
+for i in range(5):
+    a= int(input("enter a number:"))
+    numbers.append(a)
+
+#print the list 
+print("list numbers:", numbers)
