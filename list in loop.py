b4b1a28dfee3172f302672497111955573c4e747
Saba Ansari <sabaansari7110@gmail.com>
2026-01-17 17:45:14 +0530

    Update list in loop.py

--- list in loop.py
+++ list in loop.py
@@ -7,4 +7,13 @@ for i in range(5):
     numbers.append(a)
 
 #print the list 
+
 print("list numbers:", numbers)
+
+output:-
+enter a number: 3
+enter a number: 5
+enter a number: 7
+enter a number: 2
+enter a number: 9
+List number: [3,5,7,2,9,]
